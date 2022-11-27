76a0ae99ee78395410c634991651361014599dd9
stepyen <stepyen@163.com>
2022-11-27 11:49:54 +0800

    1

---
+++ app/src/main/java/com/stepyen/yuidemo/App.kt
@@ -0,0 +1,17 @@
+package com.stepyen.yuidemo
+
+import android.app.Application
+
+/**
+ * date：2022/10/28
+ * author：stepyen
+ * description：
+ *
+ */
+class App:Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
---
+++ app/src/main/java/com/stepyen/yuidemo/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.stepyen.yuidemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.stepyen.yuidemo.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
